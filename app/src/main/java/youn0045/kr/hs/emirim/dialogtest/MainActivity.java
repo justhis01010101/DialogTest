79781d1c7ea715bf1c7692e2c0eb6f65782ccf82
justhis01010101 <offentsive@naver.com>
2017-05-25 20:54:42 +0900

    Dialog의 내용 목록으로 변경하고 항목 클릭시 이벤트 처리

--- app/src/main/java/youn0045/kr/hs/emirim/dialogtest/MainActivity.java
+++ app/src/main/java/youn0045/kr/hs/emirim/dialogtest/MainActivity.java
@@ -1,17 +1,23 @@
 package youn0045.kr.hs.emirim.dialogtest;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    String[] itemArr={"저스디스","서출구","헉피","창모"};
+    Button but_dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button but_dialog = (Button)findViewById(R.id.but_dialog);
+        but_dialog = (Button)findViewById(R.id.but_dialog);
 
         but_dialog.setOnClickListener(this);
 
@@ -19,6 +25,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
+        AlertDialog.Builder dialog= new AlertDialog.Builder(this);
+
+        dialog.setTitle("내가 낳은 별");
+        dialog.setIcon(R.drawable.star);
+        //dialog.setMessage("메시지");
+
+        dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                but_dialog.setText(itemArr[i]);
+                Toast.makeText(MainActivity.this, "저즈디즈 짱짱", Toast.LENGTH_SHORT).show();
+
+            }
+        }); //선택된 항목이 바뀌었을 때
+
+        dialog.setPositiveButton("보가",null);
+        dialog.setNegativeButton("다른거", null);
+
+        dialog.show();
+
 
     }
 }
